c3e6b2f3553f798f335fdc4e94a956c0e8013b11
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-06-20 08:41:22 +0300

    adding 07_Programmer solution

---
+++ Python_OOP_Course/01_First_Steps_in_OOP_Exercise/07_Programmer.py
@@ -0,0 +1,24 @@
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if not language == self.language:
+            return f"{self.name} does not know {language}"
+
+        self.skills += skills_earned
+        return f"{self.name} watched {course_name}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills < skills_needed:
+            needed_skills = skills_needed - self.skills
+            return f"{self.name} needs {needed_skills} more skills"
+
+        if new_language == self.language:
+            return f"{self.name} already knows {self.language}"
+
+        previous_language = self.language
+        self.language = new_language
+        return f"{self.name} switched from {previous_language} to {new_language}"
