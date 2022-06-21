886d59449130311c43e4a2c5f3955a6af1aaf227
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-06-21 06:23:08 +0300

    adding 04_Glass solution

---
+++ Python_OOP_Course/02_Classes_and_Objects_Lab/04_Glass.py
@@ -0,0 +1,29 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml):
+        if self.content + ml > Glass.capacity:
+            return f"Cannot add {ml} ml"
+
+        self.content += ml
+        return f"Glass filled with {ml} ml"
+
+    def empty(self):
+        self.content = 0
+        return "Glass is now empty"
+
+    def info(self):
+        space_left = Glass.capacity - self.content
+        return f"{space_left} ml left"
+
+#
+# glass = Glass()
+# print(glass.fill(100))
+# print(glass.fill(200))
+# print(glass.empty())
+# print(glass.fill(200))
+# print(glass.info())
+#
