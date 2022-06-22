8e54071749f3d674f4b31947395c331190c3c2e4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-06-22 09:18:33 +0300

    adding 03_Account solution

---
+++ Python_OOP_Course/02_Classes_and_Objects_Exercise/03_Account.py
@@ -0,0 +1,19 @@
+class Account:
+    def __init__(self, acc_id, name, balance=0):
+        self.id = acc_id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def debit(self, amount):
+        if amount > self.balance:
+            return "Amount exceeded balance"
+
+        self.balance -= amount
+        return self.balance
+
+    def info(self):
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
