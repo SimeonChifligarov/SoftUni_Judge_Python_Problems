038ccfdba4ca35c9e01540bb33940771553db0ab
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-06-24 09:38:37 +0300

    adding 03_Word_Reverse solution

---
+++ Python_Fundamentals_Course/01_Basic_Syntax_Lab/03_Word_Reverse.py
@@ -0,0 +1,14 @@
+# Write a program that receives a single word from the user, reverses it and prints it
+
+word = input()
+
+print(word[::-1])
+
+# # Solution 2
+# word = input()
+# current_word = ""
+#
+# for char in word:
+#     current_word = char + current_word
+#
+# print(current_word)
