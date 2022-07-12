589a0ea9c2fda52521c5f0a89330b5387aa3202a
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-12 07:01:13 +0300

    adding 04_Email_Validator solution

---
+++ Python_OOP_Course/04_Encapsulation_Lab/04_Email_Validator.py
@@ -0,0 +1,19 @@
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, rest = email.split('@')
+        mail, domain = rest.split('.')
+        return all([self.__is_name_valid(name), self.__is_mail_valid(mail), self.__is_domain_valid(domain)])
