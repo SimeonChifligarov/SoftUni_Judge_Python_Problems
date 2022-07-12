20202a87d25fa09408a0a789f74dee4fd29f2d2f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-12 07:06:36 +0300

    adding 03_Football_Team_Generator mini-project solution

---
+++ Python_OOP_Course/04_Encapsulation_Exercise/03_Football_Team_Generator/project/player.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, name, sprint, dribble, passing, shooting):
+        self.name = name
+        self.__sprint = sprint
+        self.__dribble = dribble
+        self.__passing = passing
+        self.__shooting = shooting
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    def __str__(self):
+        str_result_list = [f"Player: {self.name}",
+                           f"Sprint: {self.__sprint}",
+                           f"Dribble: {self.__dribble}",
+                           f"Passing: {self.__passing}",
+                           f"Shooting: {self.__shooting}",
+                           ]
+        return "\n".join(str_result_list)
