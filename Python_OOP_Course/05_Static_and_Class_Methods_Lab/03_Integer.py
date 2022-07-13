d0bbbe2324e309e1f22edaf056707ee85605b2ba
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-13 08:44:53 +0300

    adding 03_Integer - not solved # TODO Solve this one

---
+++ Python_OOP_Course/05_Static_and_Class_Methods_Lab/03_Integer.py
@@ -0,0 +1,16 @@
+# TODO solve this one ;)
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        pass
+
+    @classmethod
+    def from_roman(cls, value):
+        pass
+
+    @classmethod
+    def from_string(cls, value):
+        pass
