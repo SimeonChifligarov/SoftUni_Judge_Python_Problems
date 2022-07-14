e8131b7ddb924e306b5ab66aff4524645849604e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-14 07:53:19 +0300

    adding 02_Groups solution

---
+++ Python_OOP_Course/06_Polymorphism_and_Abstraction_Exercise/02_Groups.py
@@ -0,0 +1,28 @@
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __repr__(self):
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name, people):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(f"{self.name} {other.name}", self.people + other.people)
+
+    def __repr__(self):
+        return f"Group {self.name} with members {', '.join([repr(p) for p in self.people])}"
+
+    def __getitem__(self, item):
+        return f"Person {item}: {repr(self.people[item])}"
