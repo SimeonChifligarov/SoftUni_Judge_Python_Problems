bfd8c8f2b8292f14a0905c2ac4a1ed0f9a33ea25
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-14 07:53:37 +0300

    adding 03_Account solution

---
+++ Python_OOP_Course/06_Polymorphism_and_Abstraction_Exercise/03_Account.py
@@ -0,0 +1,61 @@
+class Account:
+    def __init__(self, owner, amount=0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    def add_transaction(self, amount):
+        if not type(amount) == int:
+            raise ValueError("please use int for amount")
+
+        self._transactions.append(amount)
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+    @staticmethod
+    def validate_transaction(account, amount_to_add):
+        if account.balance + amount_to_add < 0:
+            raise ValueError("sorry cannot go in debt!")
+
+        account._transactions.append(amount_to_add)
+        return f"New balance: {account.balance}"
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, item):
+        return self._transactions[item]
+
+    def __reversed__(self):
+        return reversed(self._transactions)
+
+    def __add__(self, other):
+        new_account = Account(f"{self.owner}&{other.owner}", self.amount + other.amount)
+        new_account._transactions = self._transactions + other._transactions
+        return new_account
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __ne__(self, other):
+        return not self.balance == other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __le__(self, other):
+        return self.balance <= other.balance
