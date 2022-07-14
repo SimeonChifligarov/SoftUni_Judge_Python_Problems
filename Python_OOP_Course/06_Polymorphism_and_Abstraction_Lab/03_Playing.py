f6e5b5a56c0100a3d68fd390288f472360c482ce
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-14 07:50:04 +0300

    adding 03_Playing solution

---
+++ Python_OOP_Course/06_Polymorphism_and_Abstraction_Lab/03_Playing.py
@@ -0,0 +1,12 @@
+def start_playing(obj):
+    return obj.play()
+
+# # Test Code
+# class Guitar:
+#     @staticmethod
+#     def play():
+#         return "Playing the guitar"
+#
+#
+# guitar = Guitar()
+# print(start_playing(guitar))
