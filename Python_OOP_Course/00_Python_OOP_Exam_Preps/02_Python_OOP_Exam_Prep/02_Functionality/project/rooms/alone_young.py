c7a51728f685f0dd10f2c13f8854c4a5233d5f29
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-21 08:03:02 +0300

    adding 02_Functionality solution for Exam Prep #02

---
+++ Python_OOP_Course/00_Python_OOP_Exam_Preps/02_Python_OOP_Exam_Prep/02_Functionality/project/rooms/alone_young.py
@@ -0,0 +1,10 @@
+from project.appliances.tv import TV
+from project.rooms.room import Room
+
+
+class AloneYoung(Room):
+    def __init__(self, name: str, salary: float):
+        super().__init__(name, salary, 1)
+        self.room_cost = 10
+        self.appliances = [TV()]
+        self.expenses = self.calculate_expenses(self.appliances)
---
+++ Python_OOP_Course/00_Python_OOP_Exam_Preps/02_Python_OOP_Exam_Prep/02_Functionality/project/rooms/old_couple.py
@@ -0,0 +1,12 @@
+from project.appliances.fridge import Fridge
+from project.appliances.stove import Stove
+from project.appliances.tv import TV
+from project.rooms.room import Room
+
+
+class OldCouple(Room):
+    def __init__(self, name: str, pension_one: float, pension_two: float):
+        super().__init__(name, budget=pension_one+pension_two, members_count=2)
+        self.room_cost = 15
+        self.appliances = [TV(), TV(), Fridge(), Fridge(), Stove(), Stove()]
+        self.expenses = self.calculate_expenses(self.appliances)
