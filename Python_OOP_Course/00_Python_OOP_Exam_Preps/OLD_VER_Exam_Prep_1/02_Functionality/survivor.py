283c64f52a1115eedcf370aeb6642fe4c0535b20
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-07-21 08:09:19 +0300

    adding 02_Functionality solution for Exam Prep #01 (Old Version)

---
+++ Python_OOP_Course/00_Python_OOP_Exam_Preps/OLD_VER_Exam_Prep_1/02_Functionality/survivor.py
@@ -0,0 +1,58 @@
+class Survivor:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.health = 100
+        self.needs = 100
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            raise ValueError("Name not valid!")
+
+        self.__name = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            raise ValueError("Age not valid!")
+
+        self.__age = value
+
+    @property
+    def health(self):
+        return self.__health
+
+    @health.setter
+    def health(self, value):
+        if value < 0:
+            raise ValueError("Health not valid!")
+
+        self.__health = min(value, 100)
+
+    @property
+    def needs(self):
+        return self.__needs
+    
+    @needs.setter
+    def needs(self, value):
+        if value < 0:
+            raise ValueError("Needs not valid!")
+
+        self.__needs = min(value, 100)
+
+    @property
+    def needs_sustenance(self):
+        return self.needs < 100
+
+    @property
+    def needs_healing(self):
+        return self.health < 100
