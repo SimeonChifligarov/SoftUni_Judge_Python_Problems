b9de441d2e95e73a0a272892a3d2da6822602ec5
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-08-12 09:20:35 +0300

    adding 02_Chars_to_String solution

---
+++ Python_Fundamentals_Course/02_Data_Types_and_Variables_Exercise/02_Chars_to_String.py
@@ -0,0 +1,7 @@
+# Write a function that receives 3 characters. Combine all the characters into one string and print it on the console
+
+input_char_one = input()
+input_char_two = input()
+input_char_three = input()
+
+print(f'{input_char_one}{input_char_two}{input_char_three}')
